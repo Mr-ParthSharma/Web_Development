d2ff6ed5c872a73aa0f9e82231ac0e13d24990eb
Parth Sharma <62994582+Mr-ParthSharma@users.noreply.github.com>
2022-10-12 19:01:10 +0530

    Add files via upload

---
+++ JavaScript/Resume/script.js
@@ -0,0 +1,6 @@
+document.getElementById("resume").style.display == "none";
+
+document.getElementById("header").onclick = function(){
+    document.getElementById("resume").style.display = "block";
+}
+
